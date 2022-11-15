81c353369aaf685ae8a992d774136fe01107efd5
SJang1 <git@sjang.dev>
2022-11-16 00:38:07 +0900

    change source base url for kurrysocial

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'kurry-social/mastodon')
     end
 
     def source_base_url
